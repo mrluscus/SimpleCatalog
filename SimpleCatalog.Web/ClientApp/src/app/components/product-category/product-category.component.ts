df2e242716f84226b22920bbf83804f9b5b1753d
Andrei Komlev <mrluscus@gmail.com>
2018-08-01 20:30:37 +0700

    Added basic structure for Client App

---
+++ SimpleCatalog.Web/ClientApp/src/app/components/product-category/product-category.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'product-category',
+    templateUrl: './product-category.component.html'
+})
+export class ProductCategoryComponent {
+}
